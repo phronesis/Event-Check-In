026e70bf991730e7b6b83c3ece673c174367c979
Ofonime Francis <ofonimeusoro01@gmail.com>
2017-10-28 22:02:35 +0100

    [WIP] Adding Search Interface for searching code

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,21 @@
-// main project js file
+$(document).ready(function(){
+  $("input[id^='searchInput']").on("keyup", function(e) {
+    var search_input = $(this).val();
+    let search_box = encodeURIComponent(search_input);
+    let endpoint = `https://eventapp.com/users/${search_box}`;
+    let data = `<div>${search_box}</div>`;
+    $("#result").html(data);
+    //Make ajax request
+    $.get({
+      url: endpoint,
+      dataType: "json",
+      success: function(response) {
+        if(response.data) {
+          $.each(response.data.items, function(index, data) {
+            //Append the data to the list -- dropdown
+          });
+        }
+      }
+    });
+  });
+});
